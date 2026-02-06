734597af5e76791a243b0dbbe599823a5014f5bc
Nguyen Nguyen <128745593+nguyenntkn@users.noreply.github.com>
2026-02-06 13:48:01 -0400

    Added volcano plot and organized code
    Added detailed comments and sections for RNA-seq analysis using limma-voom, including metadata exploration and differential expression analysis.

--- TCGA-CHOL_test.R
+++ TCGA-CHOL_test.R
@@ -1,3 +1,17 @@
+############################################################
+## TCGA-CHOL RNA-seq Analysis
+## Paired Tumor vs Normal using limma-voom
+## Reference: https://cran.r-project.org/web/packages/easybio/vignettes/example_limma.html
+############################################################
+
+
+
+
+# ============ 0. Loading required packages ============
+# install.packages(c('data.table', 'tidyverse'))
+# BiocManager::install(c('TCGAbiolinks', 'easybio', 'SummarizedExperiment',
+#                        'DESeq2', 'edgeR', 'EnhancedVolcano'))
+
 library('TCGAbiolinks')
 library('easybio')
 library('data.table')
@@ -5,43 +19,47 @@ library('SummarizedExperiment')
 library('DESeq2')
 library('tidyverse')
 library('edgeR')
+library('EnhancedVolcano')
 
-# Following this vignette: 
-# https://cran.r-project.org/web/packages/easybio/vignettes/example_limma.html
 
-# Get query (metadata) for downloading
+
+
+
+# ============ 1. Query TCGA metadata and download count data ============
 query <- GDCquery(
   project = "TCGA-CHOL",
   data.category = "Transcriptome Profiling",
   data.type = "Gene Expression Quantification"
 )
-# ============= 1. exploring metadata =============
+
+# Download associated data. Then convert into a RangedSummarizeExperiment object.
+GDCdownload(query = query)
+data <- GDCprepare(query = query)
+
+
+
+
+
+# ============ 2. Exploring metadata to identify paired patients ============
+# Extract sample-level metadata
 metadata <- query[[1]][[1]]
-# Just checking what sample types there are
 
-
+# Check sample composition
 unique(metadata$sample_type)
-# There's only Primary Tumor and Solid Tissue Normal
-
-# Experimental strategy (RNA-seq, microarray?...)
 unique(metadata$experimental_strategy)
-# All RNA-Seq. Wonder if people still use microarray here...
-
-# Analysis workflow?
 unique(metadata$analysis_workflow_type)
-# They're all STAR - Counts. Makes sense that they use STAR aligner for RNA-seq
-# Seemed like they don't use HISAT2 aligner or Kallisto/Salmon pseudoaligner...
+# There's only Primary Tumor and Solid Tissue Normal
+# All RNA-Seq. All used STAR - Counts work flow.
+
+# Total samples and patients
+n_samples  <- length(unique(metadata$cases))
+n_patients <- length(unique(metadata$cases.submitter_id))
 
 # Get number of samples
 length(unique(metadata$cases))
-# 44 samples total
+# 44 samples total from 36 patients
 
-# Get number of patients
-length(unique(metadata$cases.submitter_id))
-# 36 patients
-
-# Since there is some miss match between patient number and samples, might be good
-# to check more closely
+# Count tumor and normal samples per patient
 sample_count <- metadata %>% 
   group_by(cases.submitter_id, sample_type) %>% 
   summarise(n_samples = n()) %>% 
@@ -49,46 +67,41 @@ sample_count <- metadata %>%
     names_from = sample_type,
     values_from = n_samples
   )
-print(sample_count)
-# Each patient have different number of tumor vs normal samples:
-#   - Tumor only (patient only has 1 tumor data)
-#   - Normal only (patient only has 1 normal data)
-#   - Paired (patient has 1 tumor and 1 normal data)
+
+# Rename columns for clarity
+colnames(sample_count) <- c("case.submitter_id", "tumor", "normal")
+
+# Keep patients with at least one tumor and one normal sample
+paired_patients <- sample_count %>%
+  filter(tumor >= 1 & normal >= 1) %>%
+  pull(case.submitter_id)
 
 
-# Might want to do two of the following approach:
-# 1. Paired analysis only: do DEA and GSEA on paired samples only 
-# 2. Supplement: treat all samples as unpaired
 
-# Get a list of patients with paired data
-# Clean up col names
-colnames(sample_count) <- c("cases.submitter_id", "tumor", "normal") 
-paired_patient <- sample_count %>% 
-  filter(tumor == 1 & normal == 1) %>% 
-  select(cases.submitter_id) %>% 
-  as.list()
 
-paired_patient <- paired_patient[[1]]
 
-# ============= 2. Download data and some more exploration ==================
-# Download associated data. 
-GDCdownload(query = query)
-
-# Convert the downloaded data into an R object suitable for analysis.
-# Since these are RNA-seq data, it will be a RangedSumarizeExperiment object
-data <- GDCprepare(query = query)
-
-# Some exploration of count data
+# ============= 3. Subset paired gene expression data  ==================
+# Extract raw count matrix (genes x samples)
 exprs_data <- assay(data, "unstranded")
-dim(exprs_data)
-# 60660 genes and 44 samples
+dim(exprs_data) # 60660 genes and 44 samples
+
+# Subset metadata to paired patients
+paired_metadata <- metadata %>% 
+  filter(cases.submitter_id %in% paired_patient) 
+
+# Clean sample_type labels
+paired_metadata$sample_type <- gsub(" ", "", paired_metadata$sample_type)
+
+# Subset count matrix to paired samples
+paired_exprs_data <- exprs_data[, paired_metadata$cases]
 
 
-# ================= 2.1. PCA =================
 
-# So we were originally taught to use PCA, but seems like it isn't the best for 
-# RNA-seq data as its assumptions don't match
 
+
+# ============ 4. Exploring expression data ============
+
+# ----------- 4.1. PCA with VST transformation ---------
 
 # Some notes before doing PCA: 
 #   1. PCA needs expression data table to be transposed (row = samples, col = feature)
@@ -97,110 +110,106 @@ dim(exprs_data)
 #      for sequencing depth. 
 #         NOTE: This is not needed for DEA since some tools requires raw count input. 
 
-# For visualization (PCA or heatmap), 3 normalization techniques are recommended
+# For visualization (PCA or heatmap), 3 normalization techniques are recommended:
 #   1. Transcript per million (TPM)
 #   2. Regularized Log (rlog) or Variance Stabilizing Transformation (VST)
 #   3. Log2(x+1)
 # Apparently it's best to use VST. BUT WHY? READ MORE INTO THIS!!
 
+# Perform PCA
+pca_data <- prcomp(paired_exprs_data %>% vst() %>% t())
 
-# PCA:
-pca_data <- prcomp(
-  exprs_data %>% vst() %>% t()        
-)
-
-# PCA data is stored in "x" within the prcomp object. This contains:
-# col: PCs
-# row: samples
+# PCA data is stored in "x" within the prcomp object (col-PCs, row-samples)
 pca_data$x %>% 
-  ggplot(aes(x=PC1, y=PC2, colour = metadata$sample_type)) + 
-  geom_point()
-
-
-# ================= 2.2. Extract paired samples =================
-
-# First do paired analysis
-# Extract paired data
-paired_metadata <- metadata %>% 
-  filter(cases.submitter_id %in% paired_patient) 
-
-# NOTE: I'm just editing directly into the metadata table rn. 
-# I want to remove all space in the sample_type column
-paired_metadata$sample_type <- gsub(" ", "", paired_metadata$sample_type)
-
-
-# Subseting expression data to contain only paired samples
-paired_exprs_data <- exprs_data[, paired_metadata$cases]
-
-# Exploratory on PCA:
-pca_paired <- prcomp(
-  paired_exprs_data %>% vst() %>% t()        
-)
-
-# PCA data is stored in "x" within the prcomp object. This contains:
-# col: PCs
-# row: samples
-pca_paired$x %>% 
   ggplot(aes(x=PC1, y=PC2, colour = paired_metadata$sample_type)) + 
   geom_point()
 
-# ======================== 2.3. Preprocessing =======================
-
-# First the count data must be stored as a DGElist object which would contain 
-# raw counts, sample information (groups, patient IDs,...), and library sizes
-# & normalization factors
-# These are all required for normalization with limma + voom
-dge_0 <- DGEList(paired_exprs_data)
-
-# We then need to calculate the normalization factor
-# NOTE: This doesn't normalize the data, just providing info for voom to normalize
-dge_0 <- calcNormFactors(dge_0)
-
-# We can then filter out the lowly expressed genes since they're not very useful
-# for differential expression, and to reduce the computational power.
-# Many people suggest filtering out by CPM (count per million) rather than raw counts
-# to account for differences in sequencing depth between samples.
-# A general rule is to choose a CPM threshold that corresponds to a raw count of 10.
-# However, it might be hard to workout which CPM value corresponds to the raw count of 10.
-# So a CPM of 1 works well for most cases.
-# (ie. count of 10 vs library size of 10M -> CPM 1)
-cpm_threshold <- 1
-genes_to_drop <- which(apply(cpm(dge_0), 1, max) < cpm_threshold)
-            # apply(): in the matrix cpm(dge), going row by row (1=row, 2=col)
-            # and get the max CPM of that row.
-dge <- dge_0[-genes_to_drop,]
-dim(dge[["counts"]])
-
-# MDS plot??
+# -------------- 4.2. MDS plot ----------------
+# WHY?
 # But need to make sure the column names in count table has the same order as
 # the samples in meta data table.
 colnames(paired_exprs_data) == paired_metadata$cases
 plotMDS(dge, col = as.numeric(as.factor(paired_metadata$sample_type)), pch = 16)
 
 
-group <- paired_metadata$sample_type
-# Voom transformation. 
-# But before that, we should specify the model to be fitted
+
+
+
+# ==================== 5. Filter lowly-expressed genes ===================
+
+# Step 1: Store raw counts in a DGEList object
+#    - Contains: raw counts, sample info (group, patient IDs), library sizes
+#    - Needed for limma-voom normalization
+dge <- DGEList(counts = paired_exprs_data)
+
+# Step 2: Calculate normalization factors
+#    - This step does NOT normalize the counts
+#    - Provides info for voom to account for library size differences
+dge <- calcNormFactors(dge)
+
+# Step 3: Filter out lowly expressed genes
+#    - Low-expression genes are usually uninformative and increase computational load
+#    - Use counts-per-million (CPM) rather than raw counts to account for library size
+#    - Here we keep genes with max CPM >= 1 (roughly equivalent to ~10 raw counts per 10M reads)
+cpm_threshold <- 1
+low_expr_genes <- which(apply(cpm(dge), 1, max) < cpm_threshold)
+dge <- dge[-low_expr_genes, ]
+
+# Step 4: Check remaining dimensions
+dim(dge$counts)
+
+
+
+# ============== 6. limma-voom differential analysis =============
+
+# Define experimental group
+group <- factor(paired_metadata$sample_type)
+# patient <- factor(paired_metadata$cases.submitter_id)
+
+# Design matrix (no intercept) 
 mm <- model.matrix(~ 0 + group)
+colnames(mm) <- levels(group)
 
-y <- voom(dge, mm, plot=T)
-# compare to the pre-filter:
-y_0 <- voom(dge_0, mm, plot=T)
+# Voom transformation. 
+v <- voom(dge, mm, plot=T)
 
-# ============== Fitting linear model with limma =============
+# Fit linear model
 fit <- lmFit(y, mm)
-# coef_table <- coef(fit)
-# colnames(coef_table) <- c("PrimaryTumor", "SolidTissueNormal")
 
-head(coef(fit))
+# Define contrast: Tumor vs Normal
+contrast <- makeContrasts(
+  PrimaryTumor - SolidTissueNormal,
+  levels = mm
+)
+
+tmp <- contrasts.fit(fit, contrast)
+tmp <- eBayes(fit2)
 
 
-contr <- makeContrasts(groupPrimaryTumor - groupSolidTissueNormal,
-                       levels = colnames(coef(fit)))
-
-
-tmp <- contrasts.fit(fit, contr)
-
-tmp <- eBayes(tmp)
 top.table <- topTable(tmp, sort.by = "P", n = Inf)
 head(top.table, 20)
+
+
+
+# ============== Volcano plot ==================
+EnhancedVolcano(top.table,
+                lab = rownames(top.table),
+                x = 'logFC',
+                y = 'P.Value',
+                pointSize = 0.5)
+
+
+
+# ========== Supplement =============
+
+# Check differences before and after filtering lowly expressed genes
+dge_0 <- DGEList(counts = paired_exprs_data)
+dge_0 <- calcNormFactors(dge_0)
+
+cpm_threshold <- 1
+low_expr_genes <- which(apply(cpm(dge_0), 1, max) < cpm_threshold)
+dge <- dge_0[-low_expr_genes, ]
+
+v_0 <- voom(dge_0, mm, plot=T)    # Pre-filter
+v <- voom(dge, mm, plot=T)        # Post-filter
+
